55bf47372cabb0a2384a211d35ea51fec1df8fd3
subhi-balaji <goldiepearl@gmail.com>
2022-06-03 11:15:55 -0700

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -233,10 +233,10 @@ const navbarselected = document.getElementsByClassName(
 );
 
 function followAboutSelected(e) {
-  if (window.scrollY >= 1009 || window.scrollY <= 1006) {
+  if (window.scrollY >= 1019 || window.scrollY <= 1016) {
     navbar.style = `color: #ffffff; text-decoration:none;`;
     return () => window.removeEventListener("scroll");
-  } else if (window.scrollY < 1010) {
+  } else if (window.scrollY < 1020) {
     return (navbar.style = `color: #f9e8fb; text-decoration: underline; text-underline-offset: 20px;`);
   }
 }
