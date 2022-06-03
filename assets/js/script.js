0f23cffc8d55c922f0ee31254a43b5b456e91448
subhi-balaji <goldiepearl@gmail.com>
2022-06-03 11:32:09 -0700

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -233,7 +233,7 @@ const navbarselected = document.getElementsByClassName(
 );
 
 function followAboutSelected(e) {
-  if (window.scrollY >= 1029 || window.scrollY <= 968) {
+  if (window.scrollY >= 978 || window.scrollY <= 968) {
     navbar.style = `color: #ffffff; text-decoration:none;`;
     return () => window.removeEventListener("scroll");
   } else {
